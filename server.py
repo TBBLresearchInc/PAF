57fe52f0c26f589d0317d50c257269dea18e0f45
Quentin Leroy <quentin.n.leroy@gmail.com>
2015-06-17 11:05:15 +0200

    ajout by quentin

--- server.py
+++ server.py
@@ -51,9 +51,8 @@ class Json:
 
     def POST(self):
         data = web.input()
-        print(data[0]["content"])
-        print(data[0]["position"])
-        return data[0]["content"]
+        print(data)
+        return data
 
 
 
