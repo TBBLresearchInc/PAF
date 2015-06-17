e383ac4bcbc4fb034540b9dfc9f59c7ea5774ccf
Quentin Leroy <quentin.n.leroy@gmail.com>
2015-06-17 10:30:38 +0200

    ajout by quentin

--- server.py
+++ server.py
@@ -1,15 +1,60 @@
 import web
 
-urls = (
-    '/py/(.*)', 'hello'
-)
-app = web.application(urls, globals())
+urls = ("/(.*)/", "Index",
+        "/py/hello/(.+)", "hello",
+        "/py/upload", "Upload",
+        "/py/json", "Json",
+        "/py/julien/(.*)", 'Julien')
+
+class Julien:
+    def GET(self, name):
+        if not name:
+            name = "Fuck"
+        return "Motherfucking Julien " + name
+
+class Index:
+    def GET(self, name):
+        if not name:
+            name = "World"
+        return "Home Page"
+
 
 class hello:
     def GET(self, name):
         if not name:
             name = 'World'
-        return 'Hello motherfucking bitch, ' + name + '!'
+        return 'Hello World ' + name
+
+class Upload:
+    def GET(self):
+        return """<html><head></head><body>
+<form method="POST" enctype="multipart/form-data" action="">
+<input type="file" name="myfile" />
+<br/>
+<input type="submit" />
+</form>
+</body></html>"""
+
+    def POST(self):
+        x = web.input(myfile={})
+        web.debug(x['myfile'].filename) # This is the filename
+        web.debug(x['myfile'].value) # This is the file contents
+        web.debug(x['myfile'].file.read()) # Or use a file(-like) object
+        print(x)
+        raise web.seeother('/py/upload')
+
+
+class Json:
+    def GET(self):
+        user_data = web.input(id = "no data")
+        return user_data.id
+
+    def POST(self):
+        data = web.input()
+        raise web.seeother('/')
+        return data
+
 
 if __name__ == "__main__":
-    app.run()
+   app = web.application(urls, globals()) 
+   app.run()
