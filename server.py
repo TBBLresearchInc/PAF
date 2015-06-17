29300e954e0ea449df1ce61fb2c4c11fe775d291
Quentin Leroy <quentin.n.leroy@gmail.com>
2015-06-17 14:42:57 +0200

    ajout by quentin

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import web
+import web, json
 
 urls = ("/(.*)/", "Index",
         "/py/json", "Json",)
@@ -17,10 +17,42 @@ class Json:
 
     def POST(self):
         data = web.input()
-        print(data["row"])
-        print(data["column"])
-        print(data["content"])
+        #print(data["row"])
+        #print(data["column"])
+        #print(data["content"])
+        grid.update(data)
+        for i in range(0, len(grid)):
+            print(grid.get(i))
         return "row : " + data["row"] + " , column : " + data["column"] + ", content : " + data["content"]
+
+class Grid:
+
+    grid = []
+
+    def __init__(self, grid):
+        self.grid = grid
+
+    def update(self, cell):
+        for i in range(0, len(self.grid)):
+            if self.grid[i]["row"] == cell["row"] & self.grid[i]["column"] == cell["column"]:
+                self.grid[i] = cell
+                return
+            else:
+                self.grid.append(cell)
+
+    def get(self, pos):
+        return self.grid[pos]
+
+    def __len__(self):
+        return len(self.grid)
+
+grid = Grid([])
+
+
+
+
+
+
 
 
 
