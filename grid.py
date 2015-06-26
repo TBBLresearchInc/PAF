3f80a84afd95b8e46ae41dbf62f5f219233dd54f
Quentin Leroy <quentin.n.leroy@gmail.com>
2015-06-26 09:24:25 +0200

    ajout by quentin

--- grid.py
+++ grid.py
@@ -1,132 +1,55 @@
-from LogicalEngine.RuleRow import RuleRow
-from logicalParse.case import Case
-from logicalParse.tab import Tab
-from logicalParse.text import Text
-
-import json
-
-__author__ = 'quentinleroy'
+__author__ = "quentinleroy"
 
 # Quentin Leroy
 # quentin.leroy@telecom-paristech.fr
-
-class GridPos:
-    pos = []
-
-    def __init__(self, row, column):
-        self.pos = [row, column]
-
-    def toStr(self):
-        return "["+str(self.pos[0])+","+str(self.pos[1])+"]"
-
-    def get_row(self):
-        return self.pos[0]
-
-    def get_column(self):
-        return self.pos[1]
-
-
 
 class Grid:
 
     """ Classe decrivant le tableau
     """
-
     grid = {}
+    grid_coords = []
 
-    grid_pos = []
-
-    colors = []
-
-    pos = GridPos(1, 1)
-
-    def __init__(self, grid = {}):
-        self.grid = grid
-        self.pos = GridPos(1,1)
+    def __init__(self):
+        self.grid = {}
+        self.grid_coords = []
 
     def update(self, row, column, content, color="wrong"):
-        self.pos = GridPos(row, column)
-        self.grid[self.pos.toStr()] = {"content": content, "row": row, "column": column, "color": color}
-        if not([row, column] in self.grid_pos):
-            self.grid_pos.append([row, column])
+        coords = (row, column)
+        self.grid[coords] = {"content": content, "row": row, "column": column, "color": color}
+        if not((row, column) in self.grid_coords):
+            self.grid_coords.append(coords)
 
-    def get_cell(self, row, column):
-        self.pos = GridPos(row, column)
-        return self.grid[self.pos.toStr()]["content"]
+    def get_cell_content(self, row, column):
+        coords = (row, column)
+        return self.grid[coords]["content"]
 
     def get_coords(self, index):
-        self.pos = GridPos(self.grid_pos[index][0], self.grid_pos[index][1])
-        return (self.grid[self.pos.toStr()]["row"], self.grid[self.pos.toStr()]["column"])
+        return self.grid_coords[index]
 
     def delete_cell(self, row, column):
-        self.pos = GridPos(row, column)
-        self.grid.__delitem__(self.pos.toStr())
-        self.grid_pos
+        coords = row, column
+        self.grid.__delitem__(coords)
+        index = 0
+        for i in range(0, len(self.grid_coords)):
+            if self.grid_coords[i] == (row, column):
+                index = i
+        self.grid_coords.pop(index)
 
     def get_color(self, row, column):
-        self.pos = GridPos(row, column)
-        return self.grid[self.pos.toStr()]["color"]
-
-    def get_pos(self):
-        return self.grid_pos
+        coords = (row, column)
+        return self.grid[coords]["color"]
 
     def set_color(self, row, column, color):
-        self.pos = GridPos(row, column)
-        self.grid[self.pos.toStr()]["color"] = color
-
-    def get_colors(self):
-        colors = {}
-        colors["cells"] = []
-        for i in range(0, len(self.grid_pos)):
-            self.pos = GridPos(self.grid_pos[i][0], self.grid_pos[i][1])
-            colors["cells"].append({"row": self.grid_pos[i][0], "column": self.grid_pos[i][1], "result": self.grid[self.pos.toStr()]["color"]})
-        return colors
-
-    def get_grid(self):
-        return self.grid
-
-    def get_grid_pos(self):
-        return self.grid_pos
+        coords = (row, column)
+        self.grid[coords]["color"] = color
 
     def nb_of_cells(self):
-        return len(self.grid_pos)
+        return len(self.grid_coords)
 
+    def toStr(self):
+        return str(self.grid)
 
-
-
-tabu = Tab({}, RuleRow([]),[] , [])
-
-
-case = Case((1,1), Text("p1"))
-tabu.add_tab(case)
-
-case = Case((1,2), Text("p2"))
-tabu.add_tab(case)
-
-case = Case((2,1), Text("p3"))
-tabu.add_tab(case)
-
-case = Case((3,1), Text("=$90A1"))
-tabu.add_tab(case)
-
-case = Case((1,0), Text("=$10A2"))
-tabu.add_tab(case)
-
-case = Case((3,3),Text("=$10B1"))
-tabu.add_tab(case)
-
-case = Case((5,9), Text("=R(A1,-A2)"))
-tabu.add_tab(case)
-
-case = Case((5,8), Text("=R(A1,-B1)"))
-tabu.add_tab(case)
-
-
-
-print(tabu.clash())
-
-
-
-
-
-
+    def reset(self):
+        self.grid = {}
+        self.grid_coords = []
--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 # coding=utf-8
-from LogicalEngine.AttitudeRow import AttitudeRow
-from LogicalEngine.PredicateRow import PredicateRow
 from LogicalEngine.RuleRow import RuleRow
 from logicalParse.case import Case
 from logicalParse.tab import Tab
@@ -13,17 +11,17 @@ __author__ = 'Quentin Leroy'
 """ Cette application web.py fait l'interface entre les inputs du client dans les cellules du tableau
  et l'intelligence de calcul du tableur (interpretation des inputs en terme d'expressions logiques puis
  divers calculs logiques), l'application renvoie aussi aux clients les resultats de la partie calcul
- (il y a t il des cases a colorier ? En quelles couleurs ? Il y a t il des cellules a completer-modifier ?
-  Il y a t il un message a donner a l utilisateur ?)"""
+ """
 
 import web, json, os.path
-
 
 from grid import Grid
 
 urls = ("/(.*)/", "Index",
         "/py/json", "Json",
         "/py/action", "Action")
+
+grid = Grid()
 
 class Index:
     def GET(self, name):
@@ -33,39 +31,20 @@ class Index:
 
 class Json:
 
-    tab_serv = Tab({}, RuleRow([]), [], [])
-
     def GET(self):
         user_data = web.input(id = "no data")
         return user_data.id
 
     def POST(self):
-        data = web.input() #retrieve input data from client
+        data = web.input() # retrieve input data from client
 
         content = str(data["content"])
         row = int(data["row"])
         column = int(data["column"])
-        grid.update(row, column, content)
+        grid.update(row, column, content)  # fill the grid with a new cell or replace the content of an existing one
 
-        tab_serv = Tab({}, RuleRow([]), [], [])
-
-        for i in range(0, grid.nb_of_cells()):
-            cur_coords = grid.get_coords(i)
-
-            case_serv = Case(cur_coords, Text(grid.get_cell(cur_coords[0], cur_coords[1])))
-
-            tab_serv.add_tab(case_serv)
-
-        ####################################
-        # data process from logical engine #
-        ####################################
-
-        # En fonction des calculs effectues, il faudra appeler set_color(row, column)
-
-        # print(str(grid.get_colors()))
-
-        print(str(json.dumps(grid.get_colors())))
-        return str(json.dumps(grid.get_colors()))  # return colors to fill the cells after some data process (not yet)
+        print(str(json.dumps(grid.toStr())))
+        return str(json.dumps(grid.toStr()))
 
 class Action:
     def GET(self):
@@ -73,27 +52,28 @@ class Action:
         return user_data.id
 
     def POST(self):
-        data = web.input() #retrieve input data from client
+        data = web.input()  # retrieve input data from client
 
         tab_serv = Tab({}, RuleRow([]), [], [])
 
         for i in range(0, grid.nb_of_cells()):
             cur_coords = grid.get_coords(i)
-            case_serv = Case(cur_coords, Text(grid.get_cell(cur_coords[0], cur_coords[1])))
-            if (grid.get_cell(cur_coords[0], cur_coords[1])) != "":
+            case_serv = Case(cur_coords, Text(grid.get_cell_content(cur_coords[0], cur_coords[1])))
+            if (grid.get_cell_content(cur_coords[0], cur_coords[1])) != "":
                 tab_serv.add_tab(case_serv)
 
         if data["action"] == "conflict":
             print("CLASH : ")
-            # print(tab_serv.clash())
             return str(json.dumps(tab_serv.clash()))
 
         if data["action"] =="solution":
             print("OPTIMIZE : ")
             return str(json.dumps(tab_serv.optimize()))
 
+        if data["action"] == "reset":
+            grid.reset()
 
-grid = Grid({})
+
 
 
 if __name__ == "__main__":
