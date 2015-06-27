7e4df7ee44c0b52ee6cba576cf1672be84149ac6
Quentin Leroy <quentin.n.leroy@gmail.com>
2015-06-27 02:13:57 +0200

    commentaires

--- grid.py
+++ grid.py
@@ -16,19 +16,31 @@ class Grid:
         self.grid = {}
         self.grid_coords = []
 
+    # Ajoute ou edite une cellule
+    # Appelee a chaque requete POST sur l'URL /py/json
     def update(self, row, column, content, color="wrong"):
+        """
+        :param row: ligne de la cellule
+        :param column: colonne de la cellule
+        :param content: chaine de caractere que contient la cellule
+        """
         coords = (row, column)
         self.grid[coords] = {"content": content, "row": row, "column": column, "color": color}
         if not((row, column) in self.grid_coords):
             self.grid_coords.append(coords)
 
+    # Renvoie le contenu d'une cellule en specifiant en arguments la ligne et la colonne
     def get_cell_content(self, row, column):
         coords = (row, column)
         return self.grid[coords]["content"]
 
+    # Renvoie les coordonnes de la cellule placee a l'adresse index
+    # Utilse quand on parcourt le tableau dans l'ordre chronologique de saisie des cellules
     def get_coords(self, index):
         return self.grid_coords[index]
 
+    # Permet de supprimer une cellule du tableau en specifiant en arguments la ligne et la colonne
+    # Utile quand l'utilisateur a rentre du texte dans une cellule mais qu'il a par la suite decide de la laisser vide
     def delete_cell(self, row, column):
         coords = row, column
         self.grid.__delitem__(coords)
@@ -46,12 +58,14 @@ class Grid:
         coords = (row, column)
         self.grid[coords]["color"] = color
 
+    # Renvoie le nombre de cellules saisies
     def nb_of_cells(self):
         return len(self.grid_coords)
 
     def toStr(self):
         return str(self.grid)
 
+    # Appelee lors du chargement de la page web cote client
     def reset(self):
         self.grid = {}
         self.grid_coords = []
